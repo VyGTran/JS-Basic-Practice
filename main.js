9e9530bdb65b7131a66e4afd9305056b02207fef
Vy Tran <vytran@Vys-MacBook-Pro.cape.k12.mo.us>
2023-09-05 13:41:32 -0500

    Exercise 1-11

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+const num = prompt("Number: ");
 // Exercise 1
+
 console.groupCollapsed("Exercise 1")
 
 console.log("Hello, World!");
@@ -22,11 +24,10 @@ console.groupEnd();
 // Exercise 4
 console.groupCollapsed("Exercise 4")
 
-const num = 5
 if(num % 2 == 0) {
-    console.log("true");
+    console.log(num + " is Even");
 } else {
-    console.log("false");
+    console.log(num + " is Odd");
 }
 console.groupEnd();
 // Exercise 5
@@ -43,4 +44,90 @@ console.groupCollapsed("Exercise 6")
 const username = prompt("Enter your name: ");
 console.log("Hi, welcome back " + username);
 
+console.groupEnd();
+// Exercise 7
+console.groupCollapsed("Exercise 7")
+function factorial(num) {
+    if (num < 0)
+    return -1;
+else if (num == 0)
+    return 1;
+    else  {
+        return (num * factorial(num - 1));
+    };
+};
+console.log(factorial(num));
+console.groupEnd();
+// Exercise 8
+console.groupCollapsed("Exercise 8")
+function leapYear(year) {
+    let leap = prompt ('What is the current year:');
+if(leap % 4 == 0) {
+console.log(leap + ' is a leap year');
+} else {
+    console.log(leap + ' is not a leap year');
+}
+}
+leapYear();
+console.groupEnd();
+// Exercise 9
+console.groupCollapsed("Exercise 9")
+let sum = 0
+function numSum(){
+    while (sum < 100) {
+    if(sum) {
+        sum = 100 * (100 + 1)/2
+        console.log(sum) 
+    }
+    sum++;
+    }  
+};
+console.log(numSum());
+console.groupEnd();
+// Exercise 10
+console.groupCollapsed("Exercise 10")
+
+function operation() {
+
+    const valNum1 = document.getElementById("num1").value;
+
+
+    const valNum2 = document.getElementById("num2").value;
+
+    const Add = parseInt(valNum1) + parseInt(valNum2);
+    console.log(Add);
+
+    const Sub = valNum1 - valNum2;
+    console.log(Sub);
+
+    const Mul = valNum1 * valNum2;
+    console.log(Mul);
+
+    const Div = valNum1 / valNum2;
+    console.log(Div)
+}
+console.groupEnd();
+// Exercise 11 
+console.groupCollapsed("Exercise 11")
+
+const string = "string";
+console.log(string)
+const number = 88;
+console.log(number)
+const boolean = true;
+console.log(boolean)
+const invalid = null;
+console.log(invalid);
+let undefine;
+console.log(undefine)
+const user = {
+    name: 'Vy',
+};
+console.log(user)
+const array = ['list1', 'list2', 'list3'];
+console.log(array);
+const valNum1 = document.getElementById("num1");
+const valNum2 = document.getElementById("num2");
+const notNumber = valNum1 * valNum2;
+console.log(notNumber);
 console.groupEnd();
